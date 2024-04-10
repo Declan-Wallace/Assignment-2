73326e48c03785946e60e55c1adc6703188bf6b9
Declan W <157408268+Declan-Wallace@users.noreply.github.com>
2024-04-10 15:26:37 -0400

    Initial commit

---
+++ Assignment #2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Assignment #2
+//
+//  Created by Declan Wallace on 4/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
