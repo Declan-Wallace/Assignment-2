98382722fb0b6b2b1fc679ba5fa83d99922c4784
Declan W <157408268+Declan-Wallace@users.noreply.github.com>
2024-04-19 19:38:07 -0400

    Created necessary IBOutlets and connections to DiamondGenerator

--- Assignment #2/ViewController.swift
+++ Assignment #2/ViewController.swift
@@ -1,19 +1,64 @@
-//
-//  ViewController.swift
-//  Assignment #2
-//
-//  Created by Declan Wallace on 4/10/24.
-//
-
+//Import necessary kits
 import UIKit
 
 class ViewController: UIViewController {
-
+    //Create IBOutlets for the View Controller Scene
+    @IBOutlet weak var inputField: UITextField!
+    @IBOutlet weak var generateButton: UIButton!
+    @IBOutlet weak var generatorLabel: UILabel!
+    @IBOutlet weak var outputField: UITextView!
+    
+    //Establish connecton to struct in other file
+    let diamondGenerator = MyDiamondGenerator()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    //Establish where diamond will be put + size
+    func generateDiamond(_ diamond: Int) {
+        let diamondGenerator = MyDiamondGenerator()
+        let diamondString = diamondGenerator.generationOf(diamond: diamond)
+        outputField.text = diamondString
+        outputField.textAlignment = .center
+        let topInset = 0
+        outputField.contentInset = UIEdgeInsets(top: CGFloat(topInset), left: 0, bottom: 0, right: 0)
+        outputField.font = UIFont.systemFont(ofSize: 18)
+    }
+    //Make the button generate diamond when pressed
+    @IBAction func generateButtonTapped(_ sender: AnyObject) {
+        guard let text = inputField.text,
+              let diamond = Int(text) else {
+            generatorLabel.text = ""
+            return
+        }
+        
+        generateDiamond(diamond)
+    }
 }
 
+extension ViewController: UITextFieldDelegate {
+   
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return false
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        guard let text = textField.text,
+              let diamond = Int(text) else {
+            return
+        }
+        
+        generateDiamond(diamond)
+    }
+    
+    @IBAction func editingChanged(_ textField: UITextField) {
+        guard let text = textField.text else {
+            generateButton.isEnabled = false
+            return
+        }
+        
+        generateButton.isEnabled = !text.isEmpty
+    }
+}
