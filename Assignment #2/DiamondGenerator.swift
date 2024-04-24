949e8974797850ac1dfb87fb9d7c8754ff308912
Declan W <157408268+Declan-Wallace@users.noreply.github.com>
2024-04-24 13:51:23 -0400

    Changed where logic goes, fixed formatting, increased range

--- Assignment #2/DiamondGenerator.swift
+++ Assignment #2/DiamondGenerator.swift
@@ -1,60 +1,18 @@
-struct MyDiamondGenerator {
-    
-    func generationOf(diamond: Int) -> String {
-        var result = ""
-        let size = diamond
-        
-        //Create responses for invalid entries
-        guard size > 1 else {
-            return "Enter a number between 2 and 21"
-        }
-        guard size < 22 else {
-            return "Enter a number between 2 and 21"
-        }
-        
-    //Create an odd diamond
-        if size % 2 != 0 {
-            var space = size / 2 // Amount of spaces needed before going to the next line
-            var stars = 1 // Amount of asterisks on the first line
-            result += "Diamond with a size of \(size):\n"
-            
-            // Create a for loop for the upper half of the diamond
-            for _ in 0..<(size / 2 + 1) {
-                result += String(repeating: " ", count: space) + String(repeating: "*", count: stars) + "\n"
-                stars += 2
-            }
-            
-            // Create variables for the second for loop for bottom half
-            space = size / 2
-            stars = size - 2
-            // Create a for loop for the bottom half of the diamond
-            for _ in 0..<(size / 2) {
-                result += String(repeating: " ", count: space) + String(repeating: "*", count: stars) + "\n"
-                stars -= 2
-            }
-        } else { //Create an even diamond
-            result += "Diamond with a size of \(size):\n"
-            // Add a singular asterisk before the for loop
-            result += String(repeating: " ", count: size) + "*"
-            
-            var space = size / 2
-            var stars = 1
-            
-            // Create a for loop for the upper half of the diamond
-            for _ in 0..<(size / 2) {
-                result += String(repeating: " ", count: space * 2) + String(repeating: "* ", count: stars - 1) + "\n"
-                stars += 2
-            }
+import Foundation
+let diamondWidth = 100
+let diamondHeight = 100
 
-            // Create a for loop for the bottom half of the diamond
-            for _ in 0..<(size / 2) {
-                result += String(repeating: " ", count: space * 2) + String(repeating: "* ", count: stars - 1) + "\n"
-                stars -= 2
-            }
-            
-            // Add the singular asterisk at the bottom
-            result += String(repeating: " ", count: size) + "* "
-            }
-        return result
-    }
+let diamondPoints = [
+    Point(),
+    Point(x: 50, y: 100),
+    Point(x: 100, y: 200),
+    Point(x: 150, y: 100)
+]
+
+let diamondShape = PolygonShape(points: diamondPoints)
+
+func setup() {
+    diamondShape.position = Point(x: 200, y: 150)
+    diamondShape.fillColor = .blue
+    scene.add(diamondShape)
 }
--- Assignment #2/ViewController.swift
+++ Assignment #2/ViewController.swift
@@ -7,6 +7,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var generateButton: UIButton!
     @IBOutlet weak var generatorLabel: UILabel!
     @IBOutlet weak var outputField: UITextView!
+    var shapeScene: ShapeScene!
     
     //Establish connecton to struct in other file
     let diamondGenerator = MyDiamondGenerator()
@@ -23,13 +24,17 @@ class ViewController: UIViewController {
         outputField.textAlignment = .center
         let topInset = 0
         outputField.contentInset = UIEdgeInsets(top: CGFloat(topInset), left: 0, bottom: 0, right: 0)
-        outputField.font = UIFont.systemFont(ofSize: 18)
+        outputField.font = UIFont.systemFont(ofSize: 17)
     }
     //Make the button generate diamond when pressed
     @IBAction func generateButtonTapped(_ sender: AnyObject) {
         guard let text = inputField.text,
               let diamond = Int(text) else {
-            generatorLabel.text = ""
+            outputField.text = "Enter a number between 2 and 21"
+            outputField.textAlignment = .center
+            let topInset = 0
+            outputField.contentInset = UIEdgeInsets(top: CGFloat(topInset), left: 0, bottom: 0, right: 0)
+            outputField.font = UIFont.systemFont(ofSize: 17)
             return
         }
         
@@ -60,5 +65,68 @@ extension ViewController: UITextFieldDelegate {
         }
         
         generateButton.isEnabled = !text.isEmpty
+    }
+}
+
+struct MyDiamondGenerator {
+    
+    func generationOf(diamond: Int) -> String {
+        var result = ""
+        let size = diamond
+        //Create responses for invalid entries
+        guard size > 1 else {
+            return "Enter a number between 2 and 23"
+        }
+        guard size < 24 else {
+            return "Enter a number between 2 and 23"
+        }
+        if diamond != Int(size) {
+            return "Enter a number"
+        }
+        
+    //Create an odd diamond
+        if size % 2 != 0 {
+            var space = size / 2 // Amount of spaces needed before going to the next line
+            var stars = 1 // Amount of asterisks on the first line
+            result += "Diamond with a size of \(size):\n"
+            
+            // Create a for loop for the upper half of the diamond
+            for _ in 0..<(size / 2 + 1) {
+                result += String(repeating: "", count: space - 1) + String(repeating: "*", count: stars) + "\n"
+                stars += 2
+            }
+            
+            // Create variables for the second for loop for bottom half
+            space = size / 2
+            stars = size - 2
+            // Create a for loop for the bottom half of the diamond
+            for _ in 0..<(size / 2) {
+                result += String(repeating: "", count: space - 1) + String(repeating: "*", count: stars) + "\n"
+                stars -= 2
+            }
+        } else { //Create an even diamond
+            result += "Diamond with a size of \(size):\n"
+            // Add a singular asterisk before the for loop
+            result += String(repeating: "", count: size) + "*"
+            
+            let space = size / 2
+            var stars = 1
+            
+            // Create a for loop for the upper half of the diamond
+            for _ in 0..<(size / 2) {
+                result += String(repeating: "", count: space - 1) + String(repeating: "* ", count: stars - 1) + "\n"
+                stars += 2
+            }
+
+            // Create a for loop for the bottom half of the diamond
+            for _ in 0..<(size / 2) {
+                result += String(repeating: "", count: space - 1) + String(repeating: "* ", count: stars - 1) + "\n"
+                stars -= 2
+            }
+            
+            // Add the singular asterisk at the bottom
+            result += String(repeating: "", count: size) + "* "
+            }
+        return result
     }
 }
