399c303d3fab54427b4257ff1ba36f390edc5aa5
Declan W <declanwallace878@gmail.com>
2024-04-24 18:24:41 -0400

    Made diamond pop up from generate button

--- Assignment #2/ViewController.swift
+++ Assignment #2/ViewController.swift
@@ -1,83 +1,34 @@
-//Import necessary kits
 import UIKit
-import Foundation
+import SpriteKit
 
 class ViewController: UIViewController {
-    //Create IBOutlets for the View Controller Scene
-    @IBOutlet weak var inputField: UITextField!
     @IBOutlet weak var generateButton: UIButton!
     @IBOutlet weak var generatorLabel: UILabel!
-    @IBOutlet weak var outputField: UITextView!
+    @IBOutlet weak var skView: SKView!
+    var scene: SKScene!
     var shapeScene: ShapeScene!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        scene = SKScene(size: view.bounds.size)
+        skView.presentScene(scene)
     }
     
-    //Establish where diamond will be put + size
-    func generateDiamond(_ diamond: Int) {
-        let diamondGenerator = MyDiamondGenerator()
-        outputField.hoverStyle?.shape = MyDiamondGenerator.diamondShape
-        outputField.textAlignment = .center
-        let topInset = 0
-        outputField.contentInset = UIEdgeInsets(top: CGFloat(topInset), left: 0, bottom: 0, right: 0)
-        outputField.font = UIFont.systemFont(ofSize: 17)
-    }
-    //Make the button generate diamond when pressed
-    @IBAction func generateButtonTapped(_ sender: AnyObject) {
-        guard let text = inputField.text,
-              let diamond = Int(text) else {
-            outputField.text = "Enter a number between 2 and 23"
-            outputField.textAlignment = .center
-            let topInset = 0
-            outputField.contentInset = UIEdgeInsets(top: CGFloat(topInset), left: 0, bottom: 0, right: 0)
-            outputField.font = UIFont.systemFont(ofSize: 17)
-            return
-        }
+    @IBAction func generateButtonTapped(_ sender: UIButton) {
+        let size: CGFloat = 200 // Default size
+        var diamondPoints = [
+            CGPoint(x: 0, y: size / 2),
+            CGPoint(x: size / 2, y: size),
+            CGPoint(x: size, y: size / 2),
+            CGPoint(x: size / 2, y: 0),
+            CGPoint(x: 0, y: size / 2) // Closing point
+        ]
         
-        generateDiamond(diamond)
-    }
-}
-
-extension ViewController: UITextFieldDelegate {
-   
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        textField.resignFirstResponder()
-        return false
-    }
-    
-    func textFieldDidEndEditing(_ textField: UITextField) {
-        guard let text = textField.text,
-              let diamond = Int(text) else {
-            return
-        }
+        let diamondShape = SKShapeNode(points: &diamondPoints, count: diamondPoints.count)
         
-        generateDiamond(diamond)
-    }
-    
-    @IBAction func editingChanged(_ textField: UITextField) {
-        guard let text = textField.text else {
-            generateButton.isEnabled = false
-            return
-        }
+        let sceneCenter = CGPoint(x: scene.size.width / 2, y: scene.size.height / 2)
+        diamondShape.position = sceneCenter
         
-        generateButton.isEnabled = !text.isEmpty
-    }
-}
-
-let usersInput = inputField.text
-    let diamondPoints = [
-        Point(x: usersInput,y: usersInput/2),
-        Point(x: usersInput/2, y: 0),
-        Point(x: 0, y: usersInput/2),
-        Point(x: usersInput/2, y: usersInput)
-    ]
-    
-    let diamondShape = PolygonShape(points: diamondPoints)
-    
-    func setup() {
-        diamondShape.position = Point(x: 200, y: 150)
-        diamondShape.fillColor = .blue
-        scene.add(diamondShape)
+        scene.addChild(diamondShape)
     }
 }
